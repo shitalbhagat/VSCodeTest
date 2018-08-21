65ed1996942cb00435c2bcf4ff4081d4ffd49892
Shital Bhagat <shitalb@synerzipune.local>
2018-08-21 20:25:30 +0530

    Add project files.

---
+++ Watcher/Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.IO;
+
+namespace Watcher
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+        protected void Application_Start()
+        {
+            Program program = new Program();
+            fileSystemWatcher.Path = @"E:\\";
+            fileSystemWatcher.Created+= new FileSystemEventHandler(program.FileChanged);
+
+            fileSystemWatcher.NotifyFilter = NotifyFilters.FileName;
+            fileSystemWatcher.Filter = "user_Details.csv";
+            fileSystemWatcher.EnableRaisingEvents = true;
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ Watcher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Watcher
+{
+    public class Program
+    {
+        public void FileChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+    }
+}
