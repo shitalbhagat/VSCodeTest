effee92307e8b8338c2b8a81fa36d5775eb7d690
Shital S. Bhagat <38552053+shitalbhagat@users.noreply.github.com>
2019-01-03 12:59:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Watcher
+{
+    public class Program
+    {
+        public void FileChanged(object source, FileSystemEventArgs e)
+        {
+            string sourceFile = e.FullPath;
+            string destinationFile = @"F:\\" + e.Name;
+
+            // To move a file or folder to a new location:
+            System.IO.File.Move(sourceFile, destinationFile);
+
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+    }
+}
